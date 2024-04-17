8d00e028c58bd2617adabbb798034a1463d27e47
Ezequiel <ezeibarra343@gmail.com>
2024-04-17 14:22:23 -0300

    Se crean los prototipos y la logica de las funciones 17/04

---
+++ funciones_estudiante.c
@@ -0,0 +1,135 @@
+/*
+    Integrantes del grupo. En caso de ser un grupo de dos integrantes, no completar el último campo.
+    Si alguno de los integrantes del grupo dejara la materia, completar de todos modos sus datos, aclarando que no entrega.
+    -----------------
+    Apellido: Ibarra
+    Nombre: Gerardo Ezequiel
+    DNI: 42471248
+    Entrega:Si
+    -----------------
+    Apellido:
+    Nombre:
+    DNI:
+    Entrega:
+    -----------------
+    (Sólo para grupos de tres integrantes)
+    Apellido:
+    Nombre:
+    DNI:
+    Entrega:
+    -----------------
+
+    Comentarios (opcionales) que deseen hacer al docente sobre el TP:
+
+*/
+
+#include "constantes.h"
+#include <stdio.h>
+#include <string.h>
+
+void negativo(int argc,char* argv[])
+{
+    if(argc > 1 && strcmp(argv[1], "--negativo") == 0)
+    {
+        printf("Entro en negativo");
+    }
+}
+
+void escala_de_grises(int argc, char* argv[])
+{
+    if(argc > 1 && strcmp(argv[1], "--escala-de-grises") == 0)
+    {
+        printf("Entro en escala-de-grises");
+    }
+}
+
+void aumentar_contraste(int argc,  char* argv[])
+{
+    if(argc > 1 && strcmp(argv[1], "--aumentar-contraste") == 0)
+    {
+        printf("Entro en aumentar-contraste");
+    }
+}
+
+void reducir_contraste(int argc,  char* argv[])
+{
+    if(argc > 1 && strcmp(argv[1], "--reducir-contraste") == 0)
+    {
+        printf("Entro en reducir-contraste");
+    }
+}
+
+void tonalidad_azul(int argc,  char* argv[])
+{
+    if(argc > 1 && strcmp(argv[1],"--tonalidad-azul") == 0)
+    {
+        printf("Entro en tonalidad-azul ");
+    }
+}
+
+void tonalidad_verde(int argc,  char* argv[])
+{
+    if(argc > 1 && strcmp(argv[1], "--tonalidad-verde") == 0)
+    {
+        printf("Entro en tonalidad-verde");
+    }
+}
+
+void tonalidad_roja(int argc,  char* argv[])
+{
+    if(argc > 1 && strcmp(argv[1], "--tonalidad-roja") == 0)
+    {
+        printf("Entro en tonalidad-roja");
+    }
+}
+
+void recortar(int argc,  char* argv[])
+{
+    if(argc > 1 && strcmp(argv[1], "--recortar") == 0)
+    {
+        printf("Entro en recortar");
+    }
+}
+
+void rotar_derecha(int argc,  char* argv[])
+{
+    if(argc > 1 && strcmp(argv[1], "--rotar-derecha") == 0)
+    {
+        printf("Entro en recortar");
+    }
+}
+
+void rotar_izquierda(int argc,  char* argv[])
+{
+    if(argc > 1 && strcmp(argv[1], "--rotar-izquierda") == 0)
+    {
+        printf("Entro en rotar-izquierda");
+    }
+}
+
+
+int solucion(int argc, char* argv[])
+{
+    int i;
+    printf("Muestro los argumentos que le llegan al main \n\n");
+    printf("Recibimos %d argumentos \n",argc);
+    for (i=0; i<argc; i++)
+    {
+        printf("%s \n",argv[i]);
+    }
+    printf("----------------------------------------- \n");
+
+    negativo(argc, argv);
+    escala_de_grises(argc, argv);
+    aumentar_contraste(argc, argv);
+    reducir_contraste(argc, argv);
+    tonalidad_azul(argc, argv);
+    tonalidad_roja(argc, argv);
+    tonalidad_verde(argc, argv);
+    recortar(argc, argv);
+    rotar_izquierda(argc, argv);
+    rotar_derecha(argc, argv);
+
+    return TODO_OK;
+}
+
---
+++ funciones_estudiante.h
@@ -0,0 +1,19 @@
+#ifndef FUNCIONES_ESTUDIANTE_H_INCLUDED
+#define FUNCIONES_ESTUDIANTE_H_INCLUDED
+
+#include "constantes.h"
+
+int solucion(int argc, char* argv[]);
+
+void negativo(int, char* []);
+void escala_de_grises(int, const char* []);
+void aumentar_contraste(int, const char* []);
+void reducir_contraste(int, const char* []);
+void tonalidad_azul(int, const char* []);
+void tonalidad_roja(int, const char* []);
+void tonalidad_verde(int, const char* []);
+void recortar(int, const char* []);
+void rotar_derecha(int, const char* []);
+void rotar_izquierda(int, const char* []);
+
+#endif // FUNCIONES_ESTUDIANTE_H_INCLUDED
